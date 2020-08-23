0491585cfb48b422728dd723d23edbe0f60b83fe
0rthodox <long.popov@gmail.com>
2020-08-23 10:17:05 +0300

    improved readability

--- work/cpptasks/code/main.cpp
+++ work/cpptasks/code/main.cpp
@@ -6,16 +6,16 @@ int main() {
     int price, temperature;
     bool hasCashback;
     const int RIGHT_WIDTH = 7, LEFT_WIDTH = 20;
-
     std::cout << "Product's name:" << std::endl;
     std::cin >> name;
     std::cout << "Product's price:" << std::endl;
     std::cin >> price;
-    std::cout << "Is cashback available for this product?\n(0 for false, any other number for true)"
-    << std::endl;
+    std::cout << "Is cashback available for this product?\n(0 for false, any other number for true)" << std::endl;
     std::cin >> hasCashback;
     std::cout << "Maximum storing temperature:" << std::endl;
     std::cin >> temperature;
+
+    std::cout << '\n' << std::string(15, '*') << " OUTPUT " << std::string(15, '*') << "\n\n";
 
     std::cout << name << std::endl;
 
